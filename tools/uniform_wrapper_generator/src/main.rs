63ed024a93f438a8dd4e437a44e4913c75988b1a
Jaeho Yi <akghxhs55@gmail.com>
2026-04-29 19:31:43 +0900

    🔨: Adjust tool usage

--- tools/uniform_wrapper_generator/src/main.rs
+++ tools/uniform_wrapper_generator/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("Usage: wrappergen <path-to-gdshader>");
+        eprintln!("Usage: uniform_wrapper_generator <path-to-gdshader>");
         std::process::exit(1);
     }
 
